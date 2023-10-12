6ba4c0e89cd967501b603f943a00eb8819db76b3
netcam22 <annettelesage@btinternet.com>
2023-10-12 15:49:53 +0100

    feat: implemented cleaned up code

--- src/index.js
+++ src/index.js
@@ -8,37 +8,17 @@
 							Result: ✅ SUCCESS
 */
 
-// Declare variables
-var x = { name: 'Bart', age: 12 };
-var y = { name: 'Lisa', age: 10 };
+const people = [
+  { name: "Bart", age: 12 },
+  { name: "Lisa", age: 10 },
+  { name: "Maggie", age: 1 },
+];
 
-// put into array
-var a = [x, y, { name: 'Maggie', age: 1 }];
-
-// create the function
-function p(p) {
-	console.log(p.name);
-}
-
-// show all the names first
-p(x);
-p(y);
-p(a[2]);
-
-var result = 0;
-
-function cmpInpt(p) {
-	// check the input matches
-	if (p.age == getInputAge(p)) {
-		// add to result
-		result += 1; // increment for each correctly input age
-	}
-}
-
-// check all the inputs match
-cmpInpt(x);
-cmpInpt(y);
-cmpInpt(a[2]);
+let result = 0;
+people.forEach(function (person) {
+  console.log(person.name);
+  result = person.age === getInputAge(person) ? (result += 1) : result;
+});
 
 // ⛔🚫 STOP TIDYING! You're done ✅
 
@@ -47,17 +27,17 @@ cmpInpt(a[2]);
 // ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
 
 // PRINT SUCCESS IF EXACTLY 2 INPUTS MATCH
-console.log(`Result: ${result === 2 ? '✅ SUCCESS' : '❌ FAIL'}`);
+console.log(`Result: ${result === 2 ? "✅ SUCCESS" : "❌ FAIL"}`);
 
 function getInputAge(person) {
-	switch (person.name) {
-		case 'Bart':
-			return 12;
-		case 'Lisa':
-			return 10;
-		case 'Maggie':
-			return '1'; // she's a baby, she's allowed to get this type wrong!
-		default:
-			return 0;
-	}
+  switch (person.name) {
+    case "Bart":
+      return 12;
+    case "Lisa":
+      return 10;
+    case "Maggie":
+      return "1"; // she's a baby, she's allowed to get this type wrong!
+    default:
+      return 0;
+  }
 }
